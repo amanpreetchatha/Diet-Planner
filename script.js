d9639734d15142943c3896191f4bcd3e579e5d09
amanpreetchatha <amanpreetchatha99@gmail.com>
2025-08-31 23:12:01 -0400

    learning datatypes and operators in js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-let myName="Amanpreet Chatha"
-console.log(myName)
+let myName="Amanpreet"
+//console.log(myName)
+
+var person;
+person = myName+" "+"Chatha";
+let greeting="hello";
+console.log(greeting, person);
+
+{
+    var anotherPerson="Emiley"
+    console.log(greeting, " ",anotherPerson);
+}
+console.log(anotherPerson);
+console.log(300==22);
+console.log(3**2);
+console.log(100%30)
+
+var score=101;
+console.log(100!=="100")
+console.log(100!="100")
+console.log(100==="100")
+console.log(100=="100")

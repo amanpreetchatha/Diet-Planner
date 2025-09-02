2aba78e858a238435e6fec64c25f2a3bca2b7a6b
amanpreetchatha <amanpreetchatha99@gmail.com>
2025-09-02 12:34:12 -0400

    learning loops

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let myName="Amanpreet"
 //console.log(myName)
 
@@ -20,3 +21,48 @@ console.log(100!=="100")
 console.log(100!="100")
 console.log(100==="100")
 console.log(100=="100")
+
+
+var currentTime=11;
+console.log(currentTime<9 || currentTime>12)
+if(currentTime < 8 || currentTime > 12){
+    console.log("time is not between 8 and 12");
+}else if(currentTime > 12 && currentTime < 18){
+    console.log("time is more than 12");
+}else if(currentTime>18){
+    console.log("time is more than 18");
+}else{
+    console.log("time is:",currentTime )
+}
+
+var y=true;
+console.clear();
+console.log(y);
+console.log(false!==y);
+
+var age=88;
+switch(age>=18 != age<=65 || age>100){
+    case true:
+        console.log("Eligible for the program");
+        break;
+    case false:
+        console.log("You are not eligible");
+        break;
+    default:
+        console.log("Not eligible");
+}
+
+
+let firstName="Amanpreet", lastName="Chatha";
+console.log("Hello",firstName.concat(lastName));
+let intro =`this person's name is: ${firstName+" "+lastName}`
+console.log(intro)
+console.log(firstName=="dvjh"?"Amanpreet":"Satinder")
+*/
+
+for(i=1;i<=10;i++){
+    for(j=1;j<=10;j++){
+        console.log(i*j)
+    }
+    
+}
